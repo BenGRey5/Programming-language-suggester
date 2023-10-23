f7cb355c88ea264a8d0cbb06713926b4ed1f5e90
BenGRey5 <146392527+BenGRey5@users.noreply.github.com>
2023-10-22 21:43:10 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,28 @@
 function checkRadio() {
-    const question1 = document.querySelector('input[name="question1"]:checked').value;
-    const question2 = document.querySelector('input[name="question2"]:checked').value;
-    const question3 = document.querySelector('input[name="question3"]:checked').value;
-    const question4 = document.querySelector('input[name="question4"]:checked').value;
-    const question5 = document.querySelector('input[name="question5"]:checked').value;
+    const question1 = document.querySelector("input[name="question1"]:checked").value;
+    const question2 = document.querySelector("input[name="question2"]:checked").value;
+    const question3 = document.querySelector("input[name="question3"]:checked").value;
+    const question4 = document.querySelector("input[name="question4"]:checked").value;
+    const question5 = document.querySelector("input[name="question5"]:checked").value;
 
     
-    let recommendation = '';
+    let recommendation = "";
 
-    if (question3 === 'true' && question4 === 'false' && question5 === 'false') {
-        recommendation = 'cSharp';
-    } else if (question3 === 'false' && question4 === 'true' && question5 === 'false') {
-        recommendation = 'javaScript';
-    } else if (question3 === 'false' && question4 === 'false' && question5 === 'true') {
-        recommendation = 'python';
+    if (question3 === "true" && question4 === "false" && question5 === "false") {
+        recommendation = "cSharp";
+    } else if (question3 === "false" && question4 === "true" && question5 === "false") {
+        recommendation = "javaScript";
+    } else if (question3 === "false" && question4 === "false" && question5 === "true") {
+        recommendation = "python";
     } else {
-        recommendation = 'neither';
+        recommendation = "neither";
     }
 
     
-    document.getElementById('neither').classList.add('hidden');
-    document.getElementById('cSharp').classList.add('hidden');
-    document.getElementById('javaScript').classList.add('hidden');
-    document.getElementById('python').classList.add('hidden');
+    document.getElementById("neither").classList.add("hidden");
+    document.getElementById("cSharp").classList.add("hidden");
+    document.getElementById("javaScript").classList.add("hidden");
+    document.getElementById("python").classList.add("hidden");
 
     
     document.getElementById(recommendation).classList.remove('hidden');
