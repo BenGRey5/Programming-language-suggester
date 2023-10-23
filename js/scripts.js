48397c2def6f2476e0404df71395e23ce7f61cb2
BenGRey5 <146392527+BenGRey5@users.noreply.github.com>
2023-10-23 08:32:15 -0700

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 function checkRadio() {
-    const question1 = document.querySelector('input[name="question3"]:checked').value;
-    const question2 = document.querySelector('input[name="question3"]:checked').value;
+    const question1 = document.querySelector('input[name="question1"]:checked').value;
+    const question2 = document.querySelector('input[name="question2"]:checked').value;
     const question3 = document.querySelector('input[name="question3"]:checked').value;
     const question4 = document.querySelector('input[name="question4"]:checked').value;
     const question5 = document.querySelector('input[name="question5"]:checked').value;
